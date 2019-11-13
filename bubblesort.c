e1f118ceadaf91fffa150002492a41a3898cce6d
SHOBHIT <vermashobhit1994@gmail.com>
2019-11-13 06:18:15 +0530

    bubblesort.c file added

---
+++ bubblesort.c
@@ -0,0 +1,98 @@
+/*program to understand bubblesort*/
+#include<stdio.h>
+
+//for memset()
+#include<string.h>
+
+#define SIZE 7
+void swap(int *x,int *y);
+void Bsort(int *arr,int n);
+void display(int *arr ,int n);
+
+
+/*here largest element goes downwards*/
+void Bsort(int *arr,int n)
+{
+	//loop runs until the element exists
+	for(int i=0;i<n;i++)
+	{
+		//count is initialised with zero for further increment
+		int count = 0;
+		
+		/*loop will execute one element less from last*/
+		for(int j=0;j<n-i-1;j++)
+		{
+			//if the first element is greater than next element 
+			//It means to swap the neighbourhood elements only when the 
+			//first element is greater than next one.
+			if(arr[j] > arr[j+1])
+			{
+				//swap function for swapping the two neighbour elements in array
+				swap(&arr[j],&arr[j+1]);
+				
+				//count for increment everytime swap happens 
+				//If in this loop swap doesn't occured that means array becomes sorted.
+				count++;
+				//printf("count = %d\n",count);	
+			}
+		}
+			//for no swap condition
+			//condition for exiting out of function when no swap occurs
+			if(count == 0)
+			{
+				return;
+			}
+		
+	}
+}
+
+//function for swapping two numbers when both are passed by reference
+void swap(int* x,int*y)
+{
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+	
+}
+
+//function for display of elements	
+void display(int *arr ,int n)
+{
+	for(int i = 0;i<n;i++)
+		printf("%d, ",*(arr+i));
+	printf("\n\n");
+}
+		
+int main()
+{
+	int n;
+	
+	printf("Enter the number of list elements\n");
+	scanf("%d",&n);
+
+	//array declared an initialised
+	int arr[n] ;
+
+	//to initialised all the elements to zero
+	memset(arr,0,n*sizeof(arr[0]));
+
+	printf("Enter the elements\n");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d,",&arr[i]);
+	}
+	
+
+	printf("The elements Entered are\n");
+	display(arr,n);
+	
+	printf("before sorting array is \n");
+	display(arr,n);
+	
+	//calling the Bsort()
+	Bsort(arr,n);
+
+	//printing array after sorting
+	printf("After sorting array is \n");
+	display(arr,n);
+}
